f473c9c35a0dd35e36fa6ba9ca73aca4198df345
Oleg Kravchenko <advkov@gmail.com>
2019-04-07 14:07:46 +0500

    Первый проект перед изменением

--- task1/main.js
+++ task1/main.js
@@ -1,52 +1,38 @@
 'use strict';
 
-console.log('Credo');
+// console.log('Credo');
+/*
+let money = prompt('Ваш бюджет за месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD');
+*/
+let money = '80000',
+    time = '2019-01-01';
 
-let money = prompt('Ваш бюджет за месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-// let money = 80000;
-// let time = '2019-04-06';
+let appData = {};
+appData.budget = money;
+appData.timeData = time;
+appData.expenses = {};
+appData.optionalExpenses = {};
+appData.income = [];
+appData.savings = false;
+// console.log(appData);
+/*
+let message1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    message2 = prompt('Во сколько обойдется?'),
+    message3 = prompt('Введите обязательную статью расходов в этом месяце'),
+    message4 = prompt('Во сколько обойдется?');
+*/
+let message1 = 'Еда',
+    message2 = '10000',
+    message3 = 'Одежда',
+    message4 = '6000';
 
-// Тестирование ввода
-// console.log('money: ', money);
-// console.log('time: ', time);
 
-// Создать объект appData, который будет содержать такие данные:
-let appData =  {};
-// ·      бюджет (передаем сюда переменную из п.2)
-    appData.budget = money;
-// ·      данные времени - timeData (передаем сюда переменную из п.2)
-    appData.timeData = time;
-// ·      объект с обязательными расходами - expenses (смотри пункт 4)
-    appData.expenses = {};
-// ·      объект с необязательными расходами - optionalExpenses (оставляем пока пустым)
-    appData.optionalExpenses = {};
-// ·      массив данных с доп. доходом - income (оставляем пока пустым)
-    appData.income = [];
-// ·      свойство savings (выставляем его как false)
-    appData.savings = false;
-console.log(appData);
 
-// 4) Задать пользователю по 2 раза вопросы:
-    // “Введите обязательную статью расходов в этом месяце”
-    // let message1 = 'Еда';
-    let message1 = prompt('Введите обязательную статью расходов в этом месяце');
-    // “Во сколько обойдется?”
-    // let message2 = '10000';
-    let message2 = prompt('Во сколько обойдется?');
-    // Записать ответы в объект expenses в формате: 
+appData.expenses.message1 = message2;
+appData.expenses.message3 = message4;
 
-    // expenses: {
-    // “ответ на первый вопрос” : “ответ на второй вопрос”
-    // }
-    appData.expenses = {message1 : message2};
-    // 5) Вывести на экран пользователя бюджет на 1 день (брать месяц за 30 дней, использовать alert)
-    let one_day = (appData.budget - appData.expenses.message1) / 30;
-    alert(one_day);
-// console.log("бюджет на 1 день: ", one_day);
-console.log(appData);
-    // 6) Проверить, чтобы все работало без ошибок в консоли
-// let ver_one_day = (80000 - 10000) / 30;
-// console.log(ver_one_day);
-    // 7) Создать свой репозиторий на GitHub и поместить туда папку с первым заданием
+// alert(appData.budget / 30);
+console.log(appData.budget / 30);
+// console.log(appData.expenses.message1);
---
+++ task1/mymain.js
@@ -0,0 +1,65 @@
+'use strict';
+
+console.log('Credo');
+
+let money = prompt('Ваш бюджет за месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+
+// let money = 80000;
+// let time = '2019-04-06';
+
+// Тестирование ввода
+// console.log('money: ', money);
+// console.log('time: ', time);
+
+// Создать объект appData, который будет содержать такие данные:
+let appData =  {};
+// ·      бюджет (передаем сюда переменную из п.2)
+    appData.budget = money;
+// ·      данные времени - timeData (передаем сюда переменную из п.2)
+    appData.timeData = time;
+// ·      объект с обязательными расходами - expenses (смотри пункт 4)
+    appData.expenses = {};
+// ·      объект с необязательными расходами - optionalExpenses (оставляем пока пустым)
+    appData.optionalExpenses = {};
+// ·      массив данных с доп. доходом - income (оставляем пока пустым)
+    appData.income = [];
+// ·      свойство savings (выставляем его как false)
+    appData.savings = false;
+console.log(appData);
+
+// 4) Задать пользователю по 2 раза вопросы:
+    // “Введите обязательную статью расходов в этом месяце”
+    // let message1 = 'Еда';
+    let message1 = prompt('Введите обязательную статью расходов в этом месяце');
+    // “Во сколько обойдется?”
+    // let message2 = '10000';
+    let message2 = prompt('Во сколько обойдется?');
+    // Записать ответы в объект expenses в формате: 
+
+    // expenses: {
+    // “ответ на первый вопрос” : “ответ на второй вопрос”
+    // }
+    appData.expenses = {message1 : message2};
+    // 5) Вывести на экран пользователя бюджет на 1 день (брать месяц за 30 дней, использовать alert)
+    let one_day = (appData.budget - appData.expenses.message1) / 30;
+    alert(one_day);
+// console.log("бюджет на 1 день: ", one_day);
+console.log(appData);
+    // 6) Проверить, чтобы все работало без ошибок в консоли
+// let ver_one_day = (80000 - 10000) / 30;
+// console.log(ver_one_day);
+    // 7) Создать свой репозиторий на GitHub и поместить туда папку с первым заданием
+
+    /*
+Вопросы к этому заданию
+Сколько типов данных существует в JS?
+
+Как вывести информацию в консоль?
+
+Какая функция операторов || и &&? 
+
+
+
+
+    */
