15616a804ee2f01e01bd0bae6e0e0932a9ea114a
Oleg Kravchenko <advkov@gmail.com>
2019-05-03 21:43:00 +0500

    add JSON

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,146 +1,154 @@
-console.log('Credo');
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
 
-	'use strict';
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
 
-	let tab = document.querySelectorAll('.info-header-tab'),
-		info = document.querySelector('.info-header'),
-		tabContent = document.querySelectorAll('.info-tabcontent');
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
 
-	function hideTabContent (a) {
-		for (let i = a; i < tabContent.length; i++) {
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	}
+    hideTabContent(1);
 
-	hideTabContent (1);
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
 
-	function showTabContent (b) {
-		if (tabContent[b].classList.contains('hide')) {
-			tabContent[b].classList.remove('hide');
-			tabContent[b].classList.add('show');	
-		}
-	}
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
 
-	info.addEventListener('click', function (event) {
-		let target = event.target;
-		if (target && target.classList.contains('info-header-tab')) {
-			for (let i = 0; i < tab.length; i++) {
-				if (target == tab[i]) {
-					hideTabContent(0);
-					showTabContent(i);
-					break;
-				}
-			}
-		}
-	});
+    });
+
+    // Timer 
+
+    let deadline = '2019-05-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+     // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+
 });
-
-// Timer
-/* 
- * Что должно быть:
- * 1. Deadline - время когда таймер должен остановиться.
- * 2. Требуется узнать: сколько времени осталось до deadline:
- * 2.1. Взять время в будущем (deadline) минус время текущее (когда пользователь зайдёт на страницу)
- * 2.2. Из этой разности требуется получить: Часы, Минуты, Секунды (которые требуется отображать на странице)
- * 2.3. Написать фунцию "настройщик", которая меняет цифры в зависимости от времени 
- * 2.4. Написань функцию "рендеринг", которая обновляет данные на странице (каждую секунду)
- */
-
-// Установка deadline
-let deadline = '2019-04-29 15:53:15'; // moment().format('YYYY-MM-DD HH:m:s')
-
-/**
- * Принимает строку deadline, вычисляет разность между deadline и текущем временем, 
- * возвращает объект: часы, минунуты, секунды из разности и саму разность. 
- */
-/**
- * 
- * @param {deadline} endtime 
- */
-function getTimeRemaining(endtime) {
-	let t = Date.parse(deadline) - Date.parse(new Date),
-		seconds = Math.floor((t / 1000) % 60),
-		minutes = Math.floor((t / 1000 / 60) % 60),
-		hours = Math.floor((t / (1000 * 60 * 60)));
-	// hours = Math.floor((t / 1000 / 60 / 60) % 24);
-	// days = Math.floor((t / (1000 * 60 * 60 * 24)));
-
-	return {
-		'total': t,
-		'hours': hours,
-		'minutes': minutes,
-		'seconds': seconds
-	};
-}
-
-// Получение элементов со страницы
-/**
- * 
- * @param {'time'} id 
- * @param {deadline} endtime 
- */
-function setClock(id, endtime) {
-	let timer = document.getElementById('timer'),
-		hours = timer.querySelector('.hours'),
-		minutes = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds');
-		timeInterval = setInterval(updateClock, 1000);
-
-	// Рендеринг значений на странице
-	function updateClock() {
-		let zero = 0;
-		let t = getTimeRemaining(endtime);
-		if (t.hours < 10) {
-			hours.textContent = zero + '' + t.hours;
-		} else {
-			hours.textContent = t.hours;
-		}
-		if (t.minutes < 10) {
-			minutes.textContent = zero + '' + t.minutes;
-		} else {
-			minutes.textContent = t.minutes;
-		}
-		if (t.seconds < 10) {
-			seconds.textContent = zero + '' + t.seconds;
-		} else {
-			seconds.textContent = t.seconds;
-		}
-
-		if (t.total <= 0) {
-			clearInterval(timeInterval);
-			hours.textContent = '00';
-			minutes.textContent = '00';
-			seconds.textContent = '00';
-		}
-	}
-}
-
-setClock('timer', deadline);
-
-
-	// Modal
-
-	let more = document.querySelector('.more'),
-		overlay = document.querySelector('.overlay'),
-		close = document.querySelector('.popup-close');
-	
-	more.addEventListener('click', function () {
-		overlay.style.display = 'block';
-		this.classList.add('more-splash');
-		// Запрет на перемещение страницы, пока открыто модальное окно.
-		document.body.style.overflow = 'hidden';
-	});
-
-	close.addEventListener('click', function() {
-		overlay.style.display = 'none';
-		more.classList.remove('more-splash');
-		document.body.style.overflow = '';
-	});
-
-
-
-
-
-console.log('Credo2');
